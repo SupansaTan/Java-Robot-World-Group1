cf39fd149553ef9db4559e178d636ef1c94dba7c
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2020-10-11 08:31:39 +0700

    Can display world from .txt file

--- Robot_World_Group_1/Robot_World.java
+++ Robot_World_Group_1/Robot_World.java
@@ -1,17 +1,51 @@
 import javax.swing.*;
+
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+import java.awt.*;
 import world.World;
 import robot.Robot;
 
 public class Robot_World {
+   static World world;
+   static JFrame frame;
+   static JPanel mainPanel;
+   static JButton[] buttons; 
+
     public static void main(String[] args)
     {
         // create frame
-        JFrame frame = new JFrame("Robot World");
+        frame = new JFrame("Robot World");
         frame.setSize(800,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        World world = new World();
-        world.display();
+        // create object of class World
+        world = new World();
+
+        // use grid in panel for divide blocks
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(world.getMaxRow(), world.getMaxColumn(),2,2));
+        mainPanel.setBackground(Color.BLACK);
+        frame.add(mainPanel);
+
+        // use button instead of each block
+        int amountOfBtn = world.getMaxRow() * world.getMaxColumn();
+        buttons = new JButton[amountOfBtn];
+
+        for (int i=0; i<amountOfBtn; i++)
+        {
+            buttons[i] = new JButton();
+            if (world.isBlackBlock(i / world.getMaxRow(), i % world.getMaxColumn()))
+            {
+                buttons[i].setBackground(Color.BLACK);
+            }
+            else
+            {
+                buttons[i].setBackground(Color.WHITE);
+            }
+            buttons[i].setBorder(null);
+            mainPanel.add(buttons[i]);
+        }
 
         frame.setVisible(true);
     }
